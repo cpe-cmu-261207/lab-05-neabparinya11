951aa01b6dbc79d4d11b141f3595fe0c49bd2202
neabparinya11 <neabparinya222@gmail.com>
2021-08-06 21:59:58 +0700

    components

---
+++ src/Components/Task.tsx
@@ -0,0 +1,18 @@
+import { textSpanContainsTextSpan } from "typescript";
+
+function TaskCard(props:any){
+    const { index,value,onDelete,onDone} = props
+    return (
+        <div
+            className="flex justify-between h-8 items-center py-6 border-b"
+          >
+            <span className="text-2xl "style={{ textDecoration: value.isDone ? "line-through" : "" }}> {value.text} </span>
+            <div className="flex space-x-1 items-center ">
+              <button className="bg-green-400 w-24 text-2xl" onClick={()=>{onDone(index)
+                console.log(index)}}>Done</button>
+              <button className="bg-red-400 w-24 text-2xl" onClick={()=>onDelete(index)}>Delete</button>
+            </div>
+          </div>
+    );
+}
+export default TaskCard;
