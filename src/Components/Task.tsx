c6e9f73972a1fa5c6ea1f152fce4c0dd218f6cd5
neabparinya11 <neabparinya222@gmail.com>
2021-08-06 23:01:31 +0700

    ha

--- src/Components/Task.tsx
+++ src/Components/Task.tsx
@@ -1,16 +1,22 @@
+import { useState } from "react";
 import { textSpanContainsTextSpan } from "typescript";
 
 function TaskCard(props:any){
     const { index,value,onDelete,onDone} = props
+    const [Check, setCheck] = useState(false)
+    
     return (
         <div
             className="flex justify-between h-8 items-center py-6 border-b"
           >
-            <span className="text-2xl "style={{ textDecoration: value.isDone ? "line-through" : "" }}> {value.text} </span>
+            <span className="text-2xl "style={{ textDecoration: value.isDone ? "line-through" : "" }}  onMouseOver={(ev)=>{setCheck(ev.bubbles);console.log(ev.bubbles)}} onMouseLeave={()=>{setCheck(false);}}> {value.text} </span>
             <div className="flex space-x-1 items-center ">
-              <button className="bg-green-400 w-24 text-2xl" onClick={()=>{onDone(index)
-                console.log(index)}}>Done</button>
-              <button className="bg-red-400 w-24 text-2xl" onClick={()=>onDelete(index)}>Delete</button>
+              <button className="bg-green-400 w-24 text-2xl "
+                style={{visibility: Check ?"visible":"hidden"}} 
+                onClick={()=>{onDone(index)}}>Done</button>
+              <button className="bg-red-400 w-24 text-2xl" 
+                style={{visibility: Check ?"visible":"hidden"}}
+                onClick={()=>onDelete(index)}>Delete</button>
             </div>
           </div>
     );
